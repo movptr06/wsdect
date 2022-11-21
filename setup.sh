47f385523a197b1accb7d401ae28bea0220480c5
movptr06 <movptr06@outlook.kr>
2022-11-22 02:09:12 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# ref: https://askubuntu.com/a/30157/8698
+if ! [ $(id -u) = 0 ]; then
+   echo "\n[-] The script need to be run as root." >&2
+   exit 1
+fi
+
+apt install yara python3 git
+
+cd /opt
+git clone https://github.com/movptr06/wsdect
+cd -
